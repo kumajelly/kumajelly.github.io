1e6459208aadedce35cd0d8485bef18160b511e9
Sean Ward <seanthomasward@gmail.com>
2023-05-24 21:42:44 -0700

    Create widget.js

---
+++ _includes/widget.js
@@ -0,0 +1,10 @@
+<script>
+  window.chatbaseConfig = {
+    chatbotId: "xBl4WrNSFTohnMoHyi5yQ",
+  }
+</script>
+<script
+  src="https://www.chatbase.co/embed.min.js"
+  id="xBl4WrNSFTohnMoHyi5yQ"
+  defer>
+</script>
