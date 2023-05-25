bf3a73072340c786e7d2d478daa91548ddb3fa22
Sean Ward <seanthomasward@gmail.com>
2023-05-24 21:46:37 -0700

    Update widget.js

--- _includes/widget.js
+++ _includes/widget.js
@@ -1,10 +1,11 @@
 <script>
-  window.chatbaseConfig = {
-    chatbotId: "xBl4WrNSFTohnMoHyi5yQ",
-  }
+  window.chatbaseConfig = {chatbotId: "xBl4WrNSFTohnMoHyi5yQ"}
 </script>
 <script
   src="https://www.chatbase.co/embed.min.js"
   id="xBl4WrNSFTohnMoHyi5yQ"
   defer>
 </script>
+
+<script>window.chatbaseConfig={chatbotId:"xBl4WrNSFTohnMoHyi5yQ"}</script>
+<script src="https://www.chatbase.co/embed.min.js" id="xBl4WrNSFTohnMoHyi5yQ" defer></script>
